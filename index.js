629f5295621f0db6940ee9ef0aac27ae61455b0b
alexpeachey <alex.peachey@gmail.com>
2015-05-20 12:28:01 -0700

    Reorganization

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var Fallout = require('./dist/fallout.js');
-var repl = require('repl');
-var replServer = repl.start({prompt: 'Fallout > '});
-replServer.context.Fallout = Fallout;
+var Fallout = require('../dist/fallout.js');
+module.exports = Fallout
